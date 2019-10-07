c4fecf537c1a94f40fbf2c184a2996c0f4e7c479
Bruno Anacleto Santos de Sousa - DATAPREVRN <bruno.anacleto@dataprev.gov.br>
2019-10-07 16:13:26 -0300

    configuring CORS w/ dynamic origin

--- src/index.js
+++ src/index.js
@@ -17,9 +17,26 @@ mongoose.connect('mongodb://localhost/meatappdb')
   .then(() => console.log('MongoDB connected…'))
   .catch(err => console.log(err))
 
+/**
+ * Configuring CORS w/ Dynamic Origin
+ * If you do not want to block REST tools or server-to-server requests, 
+ * add a !origin check in the origin function.
+ */
+let whitelist = ['http://localhost:4200']
+
+fastify.register(require('fastify-cors'), { 
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+})
+
 // Declare our first route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  reply.send({msg: 'This is CORS-enabled for a whitelisted domain.'})
 })
 
 // Register Swagger
@@ -35,7 +52,7 @@ const start = async () => {
   try {
     await fastify.listen(3000, '0.0.0.0')
     fastify.swagger()
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    fastify.log.info(`CORS-enabled web server listening on port ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
